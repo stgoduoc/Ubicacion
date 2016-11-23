e94b6d0ca7749c9164907439eefc50a977ff63de
Santiago Neira <sant.neira@profesor.duoc.cl>
2016-11-23 18:24:54 -0300

    Dirección mostrada a través de Logs

---
+++ app/src/main/java/cl/duoc/android/ubicacion/Constants.java
@@ -0,0 +1,18 @@
+package cl.duoc.android.ubicacion;
+
+/**
+ * Created by zero on 11/23/16.
+ */
+public class Constants {
+
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME =
+            "cl.duoc.android.ubicacion";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
+
+}
--- app/src/main/java/cl/duoc/android/ubicacion/MainActivity.java
+++ app/src/main/java/cl/duoc/android/ubicacion/MainActivity.java
@@ -1,22 +1,71 @@
 package cl.duoc.android.ubicacion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected Location mLastLocation;
+    private AddressResultReceiver mResultReceiver;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mAddressRequested;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    public void fetchAddressButtonHandler(View view) {
+        // Only start the service to fetch the address if GoogleApiClient is
+        // connected.
+        if (mGoogleApiClient.isConnected() && mLastLocation != null) {
+            mostrarToast("Starting IntenterService");
+            startIntentService();
+        } else {
+            mostrarToast( "Google API is not connected OR mLastLocation is NULL gapi.isConnected="+mGoogleApiClient.isConnected() );
+        }
+        // If GoogleApiClient isn't connected, process the user's request by
+        // setting mAddressRequested to true. Later, when GoogleApiClient connects,
+        // launch the service to fetch the address. As far as the user is
+        // concerned, pressing the Fetch Address button
+        // immediately kicks off the process of getting the address.
+        mAddressRequested = true;
+        updateUIWidgets();
+    }
+
+    private void updateUIWidgets() {
+    }
+
+    protected void startIntentService(){
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, mLastLocation);
+        startService(intent);
     }
 
     public void actualizarUbicacion(View view) {
@@ -38,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void usarLocalizacion(Location location) {
+        this.mLastLocation = location;
         mostrarToast("Lat:"+location.getLatitude()+" Long:"+location.getLongitude());
         actualizarEtiquetas(location.getLatitude(), location.getLongitude());
     }
@@ -69,4 +119,70 @@ public class MainActivity extends AppCompatActivity {
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // Gets the best and most recent location currently available,
+        // which may be null in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            // Determine whether a Geocoder is available.
+            if (!Geocoder.isPresent()) {
+                Toast.makeText(this, R.string.no_geocoder_available,
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (mAddressRequested) {
+                startIntentService();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        mostrarToast(connectionResult.getErrorMessage());
+    }
+
+
+    // inner class
+    class AddressResultReceiver extends ResultReceiver {
+
+        String mAddressOutput = "";
+
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        public void showToast(String s) {
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+        }
+
+        public void displayAddressOutput() {
+            showToast(mAddressOutput);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string
+            // or an error message sent from the intent service.
+            mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            displayAddressOutput();
+
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                showToast(getString(R.string.address_found));
+            }
+
+        }
+    }
+
 }
